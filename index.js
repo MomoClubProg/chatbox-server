4bfc3599b3999d65229ee8859acd3646b2f10393
matiasvlevi <matiasvlevi@gmail.com>
2022-10-04 20:55:32 -0500

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const socket = require('socket.io');
+const http = require('node:http');
+
+// Create http server and socket.io server
+const app = express();
+const server = http.createServer(app);
+const io = new socket.Server(server);
+
+// LISTENER EVENTS
+io.on('connection', (socket) => {
+  
+  socket.on('login', (data) => {
+    // User just logged in!
+  })
+
+  socket.on('sendMessage', (msg) => {
+    // Client sent a message!
+  })
+
+  /*
+  socket.on('myOtherEvent', (msg) => {
+     
+    // Do something when `myOtherEvent` is triggered by a client
+  
+  }) */
+
+})
+
+// SERVER LISTEN
+server.listen(3000, () => {
+  console.log(`http://127.0.0.1:3000`);
+})
+
